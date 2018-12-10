56509086f64fe6c04ab5661fd92759a67068b50d
Marshall Tuinier <mtuinier@gmail.com>
2018-12-10 11:41:23 -0800

    Basic Setup of GraphQL: Sever

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user is logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+};
+
+module.exports = Mutation;
