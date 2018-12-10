84391744b26bd4a1ad8cb2779da7bb0811887b07
Marshall Tuinier <mtuinier@gmail.com>
2018-12-10 12:08:07 -0800

    Apollo SSR config

--- client/components/ErrorMessage.js
+++ client/components/ErrorMessage.js
@@ -1,7 +1,43 @@
-import styled from 'styled-components';
-import React from 'react';
+import styled from "styled-components";
+import React from "react";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+
+const DisplayError = ({ error }) => {
+  if (!error || !error.message) return null;
+  if (
+    error.networkError &&
+    error.networkError.result &&
+    error.networkError.result.errors.length
+  ) {
+    return error.networkError.result.errors.map((error, i) => (
+      <ErrorStyles key={i}>
+        <p data-test="graphql-error">
+          <strong>Shoot!</strong>
+          {error.message.replace("GraphQL error: ", "")}
+        </p>
+      </ErrorStyles>
+    ));
+  }
+  return (
+    <ErrorStyles>
+      <p data-test="graphql-error">
+        <strong>Shoot!</strong>
+        {error.message.replace("GraphQL error: ", "")}
+      </p>
+    </ErrorStyles>
+  );
+};
+
+DisplayError.defaultProps = {
+  error: {}
+};
+
+DisplayError.propTypes = {
+  error: PropTypes.object
+};
+
+// -------------------- Styles -------------------- //
 
 const ErrorStyles = styled.div`
   padding: 2rem;
@@ -17,35 +53,5 @@ const ErrorStyles = styled.div`
     margin-right: 1rem;
   }
 `;
-
-const DisplayError = ({ error }) => {
-  if (!error || !error.message) return null;
-  if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
-    return error.networkError.result.errors.map((error, i) => (
-      <ErrorStyles key={i}>
-        <p data-test="graphql-error">
-          <strong>Shoot!</strong>
-          {error.message.replace('GraphQL error: ', '')}
-        </p>
-      </ErrorStyles>
-    ));
-  }
-  return (
-    <ErrorStyles>
-      <p data-test="graphql-error">
-        <strong>Shoot!</strong>
-        {error.message.replace('GraphQL error: ', '')}
-      </p>
-    </ErrorStyles>
-  );
-};
-
-DisplayError.defaultProps = {
-  error: {},
-};
-
-DisplayError.propTypes = {
-  error: PropTypes.object,
-};
 
 export default DisplayError;
